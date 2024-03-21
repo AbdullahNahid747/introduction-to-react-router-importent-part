ebea5db1fbced64f18215c51a74b0dc0853cf0de
Abdullahnahid <abdullahalnahid747@gmail.com>
2024-03-22 03:08:56 +0600

    added file

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css'
 import NavBar from '../NavBar/NavBar';
 const Header = () => {
@@ -8,9 +8,13 @@ const Header = () => {
             <h2 className='my-5 text-7xl font-bold'>My Website</h2>
             <nav className='my-5'>
                 <Link className=' font-bold' to="/">Home</Link>
-                <Link className=' font-bold' to="/users">Users</Link>
-                <Link className=' font-bold' to="/posts">Posts</Link>
+
+                <NavLink className=' font-bold' to={"/users"}>Users</NavLink>
+
+                <NavLink className=' font-bold' to="/posts">Posts</NavLink>
+
                 <Link className=' font-bold' to="/about">About</Link>
+
                 <Link className=' font-bold' to="/contact">Contact Us</Link>
             </nav>
         </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,18 @@
-import { Outlet } from "react-router-dom";
+/* eslint-disable react/no-unknown-property */
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
+    const navigation = useNavigation();
     return (
         <div>
             <Header></Header>
             <h2 className="my-5 text-xl font-semibold">This is the Home Components</h2>
-            <Outlet></Outlet>
+            {
+                navigation.state === 'loading' ? <p><span class="loading loading-spinner loading-lg"></span></p> :
+                <Outlet></Outlet>
+            }
             <Footer></Footer>
         </div>
     );
